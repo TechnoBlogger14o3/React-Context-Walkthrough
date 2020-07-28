cb6fa4d9f19ba49623f62f65376251ee196910e4
Aman Shekhar <Aman Shekhar>
2020-07-28 20:09:14 +0530

    creating-multiple-context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
-    static contextType = ThemeContext;
-    render() { 
-    console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark
-        return ( 
-            <nav style ={{ background: theme.ui, color: theme}}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                </ul>
+  // static contextType = ThemeContext;
+  render() {
+    return (
+      <AuthContext>{(authContext) => (
+        <ThemeContext.Consumer>{(themeContext) => {
+          const { isAuthenticated, toggleAuth } = authContext;
+          const { isLightTheme, light, dark } = themeContext;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <div onClick={() => toggleAuth()}>
+                { isAuthenticated ? 'Logged in' : 'Logged out' }
+              </div>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
             </nav>
-         );
-    }
+          )
+        }}</ThemeContext.Consumer>
+      )}</AuthContext>
+    );
+  }
 }
  
 export default Navbar;
